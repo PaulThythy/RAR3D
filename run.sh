802146e7c49636ee6952b7e753da9ee34f814a01
PaulThythy <paul.thi@hotmail.fr>
2024-12-19 17:35:46 +0100

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-build_type=Debug
-
-cmake -S . -B bin/$build_type -G "Ninja" -DCMAKE_BUILD_TYPE="$build_type"
-cmake --build bin/$build_type --target Reconstruction
+cmake -S . -B build/ -G "Ninja"
+cmake --build build/ --target Reconstruction
 
 #Check if the compilation was successful
 if [ $? -eq 0 ]; then
 	echo "Compilation successful. Running the programm..."
-	#Execute the raytracer binary
-	./bin/$build_type/Reconstruction
+	./build/Reconstruction
 else
     echo "Compilation failed. Please check the error messages."
 fi
